9a705f88103bf41bac67c7f25275d68ce6607dc1
ake <tamiratake@gmail.com>
2024-08-19 16:20:17 +0300

    the header stuff

--- components/headers.tsx
+++ components/headers.tsx
@@ -8,24 +8,24 @@ const Navbar: React.FC = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-black font-bold text-xs">
           <Link href='/' className='text-black'>
-            <SparklesText text="HypoGo" className='text-xl' />
+            <SparklesText text="HypoGo" className=' text-xs md:text-xl' />
           </Link>
         </div>
         <div className="flex space-x-4">
           <Link href="/job">
-            <p className="text-black text-md hover:rounded hover:bg-gray-300 p-2">Jobs</p>
+            <p className="text-black text-xs md:text-md hover:rounded hover:bg-gray-300 p-2">Jobs</p>
           </Link>
           <Link href="/ai-tools">
-            <p className="text-black text-md hover:rounded hover:bg-gray-300 p-2">AI Tools</p>
+            <p className="text-black text-xs md:text-md hover:rounded hover:bg-gray-300 p-2">AI Tools</p>
           </Link>
           <Link href='/ai-tools/study-plan'>
-            <p className="text-black text-md hover:rounded hover:bg-gray-300 p-2">Study Plan</p>
+            <p className="text-black text-xs md:text-md hover:rounded hover:bg-gray-300 p-2">Study Plan</p>
           </Link>
           <Link href='/scholarship'>
-            <p className="text-black text-md hover:rounded hover:bg-gray-300 p-2">Scholarships</p>
+            <p className="text-black text-xs md:text-md hover:rounded hover:bg-gray-300 p-2">Scholarships</p>
           </Link>
           <Link href='/resources'>
-            <p className="text-black text-md hover:rounded hover:bg-gray-300 p-2">Resources</p>
+            <p className="text-black text-xs md:text-md hover:rounded hover:bg-gray-300 p-2">Resources</p>
           </Link>
         </div>
         <div className="flex items-center gap-4">
